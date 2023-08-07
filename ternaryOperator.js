8cd0726c509bde87c6f118b8f4e821148187b567
zrakib-dev <dev.zrakib@gmail.com>
2023-08-07 11:42:01 +0600

    ternary opetator

---
+++ ternaryOperator.js
@@ -0,0 +1,8 @@
+let age = 20;
+let message = age >= 18 ? "you can drive" : "you cannot drive";
+
+console.log(message);
+
+let speed = 190;
+let warning = speed >= 120 ? "Too Fast" : speed >= 80 ? "Fast" : "OK";
+console.log(warning);
